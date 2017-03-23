5fb6a193c067d6a1a0a022ff51aa00748e6e3475
Emazing1 <Emazing24@gmail.com>
2017-03-22 20:24:04 -0400

    modified database adding login screen

--- models/angular.js
+++
@@ -1,8 +0,0 @@
-/ node module exports the string 'animate-change' for convenience 
-angular.module('myApp', [
-  require('angular-animate-change')
-]);
-// otherwise, include the code first then the module name 
-angular.module('myApp', [
-  'angular-animate-change'
-]);
