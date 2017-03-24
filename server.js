453633f15ceceaa2e4a61b452c8c48f3250dec09
Emazing1 <Emazing24@gmail.com>
2017-03-24 11:48:24 -0400

    modified handlebars retested server with authentication

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ var http = require('http');
 var hb = require('handlebars');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
-
+var mongo = require('mongodb');
+/*var expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+*/
 
 
 // create the app
@@ -41,6 +48,81 @@ app.set("view engine", "ejs");
 
 // method override
 app.use(methodOverride("_method"));
+
+
+
+// Sign in and Registration  
+//*****Had to revert back to just the server sign in and resgistration code is conflicting with server*********
+
+/*var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+*/
+
+
 
 
 
